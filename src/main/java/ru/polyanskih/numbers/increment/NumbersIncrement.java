552e364573fc06a071b818ec3a39fae2edd981a7
dmitry.polyanskih <snickerswow@gmail.com>
2025-03-19 23:58:50 +0300

    Initial commit

---
+++ src/main/java/ru/polyanskih/numbers/increment/NumbersIncrement.java
@@ -0,0 +1,13 @@
+package ru.polyanskih.numbers.increment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumbersIncrement {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumbersIncrement.class, args);
+	}
+
+}
